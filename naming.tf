f35405c258c99ef5b4aa6945778e728412381b4e
Yamkela Kutu <yamkela@hakkutu.dev>
2025-06-27 18:31:48 +0200

    Initial local variables

---
+++ naming.tf
@@ -0,0 +1,24 @@
+locals {
+  environment = terraform.workspace
+  common_tags = {
+    Region      = var.region
+    Business    = var.business_name
+    DeployedBy  = var.deployed_by
+    ManagedBy   = "Terraform"
+    Environment = local.environment
+    Application = var.application_name
+  }
+
+  region_shorthand = {
+    "af-south-1" = "afs1"
+  }
+
+  base_name = join("-", [
+    local.region_shorthand[var.region],
+    local.environment,
+    var.business_name,
+    var.application_name,
+  ])
+
+  temp_dir = "/tmp"
+}
