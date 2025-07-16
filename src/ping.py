c251e94a18cc9c71b75cfec06d5ba59d2db7de44
Yamkela Kutu <yamkela@hakkutu.dev>
2025-07-16 17:26:21 +0200

    Add ping lambda function

---
+++ src/ping.py
@@ -0,0 +1,38 @@
+from os import environ
+from json import dumps
+from logging import Logger, getLogger
+
+############
+# Variables
+############
+log_level: str = environ.get("LOG_LEVEL", "DEBUG")
+
+################
+# Logging setup
+#################
+logger: Logger = getLogger(__name__)
+logger.setLevel(log_level)
+
+#####################
+# Lambda entry point
+#####################
+def lambda_handler(event, context):
+     signature_verified = event.get("signature_verified", False)
+     interaction_type = event.get("interaction_type", 1)
+     status_code = event.get("statusCode", 401)
+
+     logger.info(f"Event: {dumps(event, indent = 2)}")
+
+     if (
+          signature_verified and
+          interaction_type == 1 and
+          status_code in [200, 201]
+     ):
+          return {
+               "statusCode": 200,
+               "headers": {"Content-Type": "application/json"},
+               "body": dumps({"type": 1})
+          }
+
+     else:
+          return {"statusCode": 401, "body": "invalid request signature"}
