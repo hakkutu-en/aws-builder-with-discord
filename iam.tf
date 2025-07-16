23f1437dc565751f4b355e17f8446e2a2b3e0019
Yamkela Kutu <yamkela@hakkutu.dev>
2025-07-16 17:35:20 +0200

    Add ping role and policy definitions

---
+++ iam.tf
@@ -0,0 +1,59 @@
+###################
+# Ping interaction
+###################
+resource "aws_iam_role" "ping" {
+  name = "${local.base_name}-ping"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    Name = "${local.base_name}-ping"
+  })
+}
+
+resource "aws_iam_policy" "ping" {
+  name        = aws_iam_role.ping.name
+  description = "IAM policy for ping lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = [
+          aws_cloudwatch_log_group.ping.arn
+        ]
+      }
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    Name = aws_iam_role.ping.name
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ping" {
+  role       = aws_iam_role.ping.name
+  policy_arn = aws_iam_policy.ping.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ping_lambda_basic_exec" {
+  role       = aws_iam_role.ping.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
